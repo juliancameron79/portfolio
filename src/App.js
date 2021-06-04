7fefe548a7c73b75960154989350180d7758042a
juliancameron79 <juliancameron79@gmail.com>
2021-06-05 00:33:36 +0200

    Footer content completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <AboutMe></AboutMe>
       <Skills></Skills>
       <Contacts></Contacts>
+      <Footer></Footer>
     </>
   );
 }
