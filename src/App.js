345edb8a24c48a5ca49e58150449af9fb1380f70
juliancameron79 <juliancameron79@gmail.com>
2021-06-04 21:54:11 +0200

    contacts completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <AboutMe></AboutMe>
       <Skills></Skills>
+      <Contacts></Contacts>
     </>
   );
 }
